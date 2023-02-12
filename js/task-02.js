dcdb13091c357941e8cceb87fc35a872bb480241
Svitlana Vasylkovska <vaselisssa@gmail.com>
2023-02-13 00:37:01 +0200

    task 1.2

--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,21 @@
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+   "Potatoes",
+   "Mushrooms",
+   "Garlic",
+   "Tomatos",
+   "Herbs",
+   "Condiments",
 ];
+
+const ingredientsList = document.querySelector("#ingredients");
+
+const ingredientsListItems = [];
+
+ingredients.forEach((ingredient) => {
+   const listItem = document.createElement("li");
+   listItem.textContent = ingredient;
+   listItem.classList.add("item");
+   ingredientsListItems.push(listItem);
+});
+
+ingredientsList.append(...ingredientsListItems);
